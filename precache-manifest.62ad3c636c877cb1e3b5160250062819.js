570063b3df01e0af1f839afbcd1838c61d8e08e8
spury92 <bot1412@yandex.ru>
2020-01-01 18:37:23 +0300

    Updates

---
+++ precache-manifest.62ad3c636c877cb1e3b5160250062819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04a6961badb737475b14ba340f11ab0",
+    "url": "/react-commerce/index.html"
+  },
+  {
+    "revision": "63f3f20ab07298c91406",
+    "url": "/react-commerce/static/css/main.b1ba9fa6.chunk.css"
+  },
+  {
+    "revision": "8682bc5d6c917fbe8a6e",
+    "url": "/react-commerce/static/js/2.673ce343.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-commerce/static/js/2.673ce343.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63f3f20ab07298c91406",
+    "url": "/react-commerce/static/js/main.b7f663b0.chunk.js"
+  },
+  {
+    "revision": "5abcf2d4763bca828f30",
+    "url": "/react-commerce/static/js/runtime-main.f4bd0225.js"
+  }
+]);
